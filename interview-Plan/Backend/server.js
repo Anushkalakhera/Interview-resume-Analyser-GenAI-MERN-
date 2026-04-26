1b33fac0f0cede915ddcaf7a7a73ea06adbcfc56
Anushka Lakhera <160614406+Anushkalakhera@users.noreply.github.com>
2026-04-26 18:55:51 +0530

    Update server.js

--- interview-Plan/Backend/server.js
+++ interview-Plan/Backend/server.js
@@ -2,10 +2,10 @@ require("dotenv").config();
 const app = require("./src/app");
 const connectToDB = require("./src/config/database");
 
-connectToDB();
-
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+connectToDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
 });
